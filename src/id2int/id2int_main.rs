6930adb7822ea5753a0c653ca97fd2b712f744cc
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2023-04-10 19:58:15 +0200

    Functionality implemented, but not writing the graph. TODO change graph-reader

--- src/id2int/id2int_main.rs
+++ src/id2int/id2int_main.rs
@@ -4,11 +4,13 @@ use clap::ArgMatches;
 use gfa_reader::{Gfa, NEdge, NNode, NPath};
 
 pub fn id2int_main(matches: &ArgMatches, graph: &Gfa){
-    eprintln!("Running core analysis");
+    eprintln!("ID2INT");
     eprintln!("Convert non digit id to integer and compress id space");
 
     let dict: HashMap<String, u32> = HashMap::new();
-
+    let (mut ht, nnodes) = make_hashtable(graph);
+    let ne = edges_new(graph, &ht);
+    let np = path_new(graph, &ht);
 
 
     // Make a meta file
@@ -30,7 +32,7 @@ fn make_hashtable(graph: &Gfa) -> (HashMap<&String, u32>, HashMap<u32, NNode>){
 
 /// Edges to int edges
 /// Same as above
-fn edges_new(graph: Gfa, id2int: HashMap<&String, u32>) -> Vec<NEdge>{
+fn edges_new(graph: &Gfa, id2int: &HashMap<&String, u32>) -> Vec<NEdge>{
     let mut new_edges : Vec<NEdge>= Vec::new();
     for edge in graph.edges.iter(){
         let from = id2int.get(&edge.from).unwrap();
@@ -41,7 +43,7 @@ fn edges_new(graph: Gfa, id2int: HashMap<&String, u32>) -> Vec<NEdge>{
     new_edges
 }
 
-fn path_new(graph: Gfa, id2int: HashMap<&String, u32>){
+fn path_new(graph: &Gfa, id2int: &HashMap<&String, u32>){
     let mut new_path = Vec::new();
     for path in graph.paths.iter(){
         let p: Vec<u32> = path.nodes.iter().map(|n|id2int.get(n).unwrap().clone()).collect();
