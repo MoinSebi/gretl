585d02dea55e66c9aecaf29aaad9e0540b827530
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2023-04-10 19:53:21 +0200

    new edges, new paths.

--- src/id2int/id2int_main.rs
+++ src/id2int/id2int_main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 use clap::ArgMatches;
-use gfa_reader::{Gfa, NNode};
+use gfa_reader::{Gfa, NEdge, NNode, NPath};
 
 pub fn id2int_main(matches: &ArgMatches, graph: &Gfa){
     eprintln!("Running core analysis");
@@ -17,15 +17,35 @@ pub fn id2int_main(matches: &ArgMatches, graph: &Gfa){
 }
 
 /// Check if you destroy the order
-fn make_hashtable(graph: &Gfa) -> (HashMap<&str, u32>, HashMap<u32, NNode>){
-    let mut result: HashMap<&str, u32> = HashMap::new();
+fn make_hashtable(graph: &Gfa) -> (HashMap<&String, u32>, HashMap<u32, NNode>){
+    let mut result: HashMap<&String, u32> = HashMap::new();
     let mut nodes_new: HashMap<u32, NNode> = HashMap::new();
     for (i, node) in graph.nodes.iter().enumerate(){
         result.insert(node.0, i as u32);
-        nodes_new.insert(i as u32, NNode{len: node.1.len, id: i as u32, seq: node.1.seq.clone()})
+        nodes_new.insert(i as u32, NNode{len: node.1.len, id: i as u32, seq: node.1.seq.clone()});
     }
     (result, nodes_new)
 
 }
 
-fn edges_new(graph: Gfa, id2int: HashMap<&str, uy32>)
+/// Edges to int edges
+/// Same as above
+fn edges_new(graph: Gfa, id2int: HashMap<&String, u32>) -> Vec<NEdge>{
+    let mut new_edges : Vec<NEdge>= Vec::new();
+    for edge in graph.edges.iter(){
+        let from = id2int.get(&edge.from).unwrap();
+        let to = id2int.get(&edge.from).unwrap();
+        new_edges.push(NEdge{from: *from, to: *to, from_dir: edge.from_dir, to_dir: edge.to_dir})
+
+    }
+    new_edges
+}
+
+fn path_new(graph: Gfa, id2int: HashMap<&String, u32>){
+    let mut new_path = Vec::new();
+    for path in graph.paths.iter(){
+        let p: Vec<u32> = path.nodes.iter().map(|n|id2int.get(n).unwrap().clone()).collect();
+        new_path.push(NPath{name: path.name.clone(), nodes: p, dir: path.dir.clone()})
+    }
+
+}
