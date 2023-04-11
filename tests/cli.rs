c5e7956365b7896a4ce68707738ba52b5f5396ca
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2023-04-11 18:12:03 +0200

    Test function for core.

---
+++ tests/cli.rs
@@ -0,0 +1,17 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("gfastats")?;
+    cmd
+        .arg("--gfa")
+        .arg("/home/svorbrugg/code/bvd/data/example_data/testGraph.gfa")
+        .arg("--output")
+        .arg("/home/svorbrugg/code/gfastats/test1 ")
+        .arg("core");
+
+    cmd.assert().success();
+    Ok(())
+}
