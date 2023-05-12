341e80744851d17047da3d9a4aaab056753ee536
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2023-05-12 13:16:49 +0200

    Updated test

--- tests/cli.rs
+++ tests/cli.rs
@@ -94,3 +94,37 @@ fn node_list_path() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert().success();
     Ok(())
 }
+
+
+#[test]
+fn node_bootstrap() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("gfastats")?;
+    cmd
+        .arg("bootstrap")
+        .arg("--gfa")
+        .arg("/home/svorbrugg/code/bvd/data/example_data/testGraph.gfa")
+        .arg("--output")
+        .arg("/home/svorbrugg/code/gfastats/data/test/test.stats.bootstrap.txt")
+        .arg("--meta")
+        .arg("testmeta.txt");
+    cmd.assert().success();
+    Ok(())
+}
+
+
+#[test]
+fn node_bootstrap2() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("gfastats")?;
+    cmd
+        .arg("bootstrap")
+        .arg("--gfa")
+        .arg("/home/svorbrugg/code/bvd/data/example_data/testGraph.gfa")
+        .arg("--output")
+        .arg("/home/svorbrugg/code/gfastats/data/test/test.stats.bootstrap2.txt")
+        .arg("--level")
+        .arg("2");
+    cmd.assert().success();
+    Ok(())
+}
+
+
