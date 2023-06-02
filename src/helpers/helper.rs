f9e8b06841656ade89a66c07fd2e6e9d2422e2c4
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2023-06-02 14:46:13 +0200

    u32 -> String for path similarity.

--- src/helpers/helper.rs
+++ src/helpers/helper.rs
@@ -1,4 +1,6 @@
+use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
+use gfa_reader::Gfa;
 
 fn calculate_average<T>(v: &[T]) -> Option<f64>
     where T:  Into<f64> + Copy + Debug
@@ -16,4 +18,20 @@ fn calculate_average<T>(v: &[T]) -> Option<f64>
     }
 
     Some(mean)
+}
+
+
+pub fn core1(graph: &Gfa) -> HashMap<&String, u32>{
+    let mut count: HashMap<&String, u32> = HashMap::new();
+    for x in &graph.nodes{
+        count.insert(x.0, 0);
+    }
+
+    for p in graph.paths.iter(){
+        let v: HashSet<_> = p.nodes.iter().cloned().collect();
+        for y in v.iter(){
+            *count.get_mut(y).unwrap() += 1;
+        }
+    }
+    count
 }
--- src/path_similarity/stats.rs
+++ src/path_similarity/stats.rs
@@ -1,23 +1,53 @@
+use std::collections::HashMap;
 use gfa_reader::{Gfa, GraphWrapper};
-use crate::stats::helper::{core1, core2};
+use crate::stats::helper::{core1};
 
 
 /// Compute the amount of sequence in each similarity level
-pub fn accession2level(graph: &Gfa, graph2: &GraphWrapper) -> Vec<(String, Vec<(usize, usize)>)>{
-    let cores = core2(graph2, graph);
+pub fn accession2level(graph: &Gfa, graph_wrapper: &GraphWrapper) -> Vec<(String, Vec<(u32, u32)>)>{
+    let cores = core1(graph);
+    let metric_maxval = cores.values().max().unwrap();
+
 
     let mut result = Vec::new();
-    for string2path in graph2.genomes.iter(){
 
-        let mut vv = vec![(0,0); graph2.genomes.len()+1];
+
+
+    for string2path in graph_wrapper.genomes.iter(){
+
+        let mut result_temp = hashmap_zero(*metric_maxval as usize);
         for path in string2path.1.iter() {
             for node in path.nodes.iter() {
-                let level = cores[&node.parse::<u32>().unwrap()] as usize;
-                vv[level].0 += 1;
-                vv[level].1 += graph.nodes.get(node).unwrap().len;
+                let metric_value = cores[&node] as usize;
+                result_temp.get_mut(&metric_value).unwrap().0 += 1;
+                result_temp.get_mut(&metric_value).unwrap().1 += graph.nodes.get(node).unwrap().len as u32;
             }
         }
-        result.push((string2path.0.clone(), vv));
+        // Convert hashmap to vec + add to result
+        let result_vec = hashmap2vec(&result_temp);
+        result.push((string2path.0.clone(), result_vec));
     }
     return result
 }
+
+/// Create hashmap from 0 to n with (0,0) as value
+pub fn hashmap_zero(n: usize) -> HashMap<usize, (u32, u32)>{
+    let mut hashmap: HashMap<usize, (u32, u32)> = HashMap::with_capacity(n);
+
+    for i in 0..=n {
+        hashmap.insert(i, (0,0));
+    }
+    return hashmap
+}
+
+/// HashMap to Vec with (u32, u32) as value and sequential keys (0,1,2,3...)
+pub fn hashmap2vec(hashmap: &HashMap<usize, (u32, u32)>) -> Vec<(u32, u32)>{
+    let mut result = Vec::with_capacity(hashmap.len());
+    for i in 0..hashmap.len() {
+        result.push(hashmap[&i]);
+    }
+    return result
+}
+
+
+
