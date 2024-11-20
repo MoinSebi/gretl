22a4549e623f8476311317844ad7155169cabb16
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2024-11-20 16:37:11 +0100

    ps output change (thanks Ilja)

--- scripts/ps.py
+++ scripts/ps.py
@@ -7,122 +7,53 @@ import numpy as np
 import sys
 import argparse
 
-
 def read_data(filename: str) -> pd.DataFrame:
     """
-    Read the path similarity output in as DataFrame
 
-    :param filename: Input file name (or stdin of "-")
+    :param filename: Gretl bootstrap output in TSV format
     :return: Data in pandas DataFrame
     """
+
     if filename == "-": # Read from stdin
         df = pd.read_csv(sys.stdin, sep = "\t")
-
         return df
     else:
-        df = pd.read_csv(filename, sep = "\t")
+        return pd.read_csv(filename, sep = "\t")
 
-        return df
 
-def index_df(df: pd.DataFrame) -> (int, int):
+def table_sep(df: pd.DataFrame, what: str) -> pd.DataFrame:
+
+    df = df.loc[df["Type"] == what]
+    return df
+
+def plot_node_stats(df: pd.DataFrame, outfile: str, label1: str, what: str) -> None:
     """
-    Identify the index of the start and end of the sequence node part of the output
+    Plot the saturation/bootstrapping statistics for the nodes (scatterplot)
 
-    :param df: Data in pandas DataFrame
-    :return:
-    """
-
-    # Sequence starts here
-    seq_index = [i for i, x in enumerate(list(df.columns)) if x.startswith("Seq")][0]
-
-    # Data starts here
-    start_index = [i for i, x in enumerate(list(df.columns)) if x.startswith("Node")][0]+1
-
-    return seq_index, start_index
-
-
-def extract_node_df(df: pd.DataFrame, seq_index: int, start_index: int) -> pd.DataFrame:
-    """
-    Extract the node statistics from the DataFrame
-
-    :param df:
-    :param seq_index:
-    :param start_index:
-    :return:
-    """
-
-    node_soft = df.apply(lambda x: sum([x for x in x.iloc[start_index:seq_index].values if not np.isnan(x)][1:-1]), axis = 1)
-    node_core = df.apply(lambda x: [x for x in x.iloc[start_index:seq_index].values if not np.isnan(x)][-1], axis = 1)
-    node_priv = df.apply(lambda x: [x for x in x.iloc[start_index:seq_index].values if not np.isnan(x)][0], axis = 1)
-    node_pan = df.apply(lambda x: sum([x for x in x.iloc[start_index:seq_index].values if not np.isnan(x)][:]), axis = 1)
-
-    # Create a DataFrame
-    df2 = pd.DataFrame([node_core, node_soft, node_priv]).T
-
-    # Change index and column name
-    df2.columns = ["Core", "Soft", "Private"]
-    df2.index = df["Accession"]
-
-    return df2
-
-def plot_node_stats(df2: pd.DataFrame, outfile: str) -> None:
-    """
-    Plot the node core, soft and private statistics
-
-    :param df2: Core, shell and private data in pandas DataFrame
+    :param df: Gretl bootstrap output in pandas DataFrame
+    :param seq_index: Index of start of sequence
+    :param start_index: Index of start of nodes
     :param outfile: Output file name
     :return: Plot (PDF)
     """
 
-    #Plot the nodes
-    (df2/1000).plot(kind = "barh", stacked = True, figsize = (6,5), width = 0.75, legend = True,
-                    edgecolor = "black",
-                    linewidth = 0.5,
-                    color = ["royalblue", "tomato", "gold"])
-    plt.xlabel("#Nodes [x1000]")
-    plt.ylabel("Accession")
-    plt.tight_layout()
-    plt.savefig(outfile + ".ps.node.pdf")
-
-
-# Sequence
-def sub_df2(df: pd.DataFrame, seq_index: int) -> pd.DataFrame:
-    """
-    Extract the sequence statistics from the DataFrame
-
-    :param df: Data in pandas DataFrame
-    :param seq_index: Index of the sequence part of the output
-    :return: Extracted data in pandas DataFrame
-    """
-
-    seq_soft = df.apply(lambda x: sum([x for x in x.iloc[seq_index:].values if not np.isnan(x)][2:-1]), axis = 1)
-    seq_core = df.apply(lambda x: [x for x in x.iloc[seq_index:].values if not np.isnan(x)][-1], axis = 1)
-    seq_priv = df.apply(lambda x: [x for x in x.iloc[seq_index:].values if not np.isnan(x)][1], axis = 1)
-
-    # Read in DataFrame
-    df2 = pd.DataFrame([seq_core, seq_soft, seq_priv]).T
-
-    # Change index and column name
+    collen = len(df.columns)
+    node_soft = df.apply(lambda x: sum([x for x in x.iloc[3:]][1:-1]), axis = 1)
+    node_core = df.apply(lambda x: [x for x in x.iloc[3:]][-1], axis = 1)
+    node_private = df.apply(lambda x: x.iloc[3], axis = 1)
+    df2 = pd.DataFrame([node_core, node_soft, node_private]).T
     df2.columns = ["Core", "Soft", "Private"]
+    print(df2)
     df2.index = df["Accession"]
-    return df2
 
-def plot_seq_stats(df2, outfile):
-    """
-    Plot the node core, soft and private statistics
-
-    :param df2: Core, shell and private data in pandas DataFrame
-    :param outfile: Output file name
-    :return: Plot (PDF)
-    """
-    (df2/1000).plot(kind = "barh", stacked = True, figsize = (6,5), width = 0.75, legend = True,
+    (df2).plot(kind = "barh", stacked = True, figsize = (6,5), width = 0.75, legend = True,
                     edgecolor = "black",
                     linewidth = 0.5,
                     color = ["royalblue", "tomato", "gold"])
-    plt.xlabel("Sequence [kbp]")
+    plt.xlabel(label1)
     plt.ylabel("Accession")
     plt.tight_layout()
-    plt.savefig(outfile + ".ps.sequence.pdf")
+    plt.savefig(outfile + ".ps." + what + ".pdf")
 
 
 if __name__ == "__main__":
@@ -132,8 +63,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     df = read_data(args.input)
-    seq_index, start_index = index_df(df)
-    df2 = extract_node_df(df, seq_index, start_index)
-    plot_node_stats(df2, args.output)
-    df2 = sub_df2(df, seq_index)
-    plot_seq_stats(df2, args.output)
+    data = read_data(args.input)
+    df_node = table_sep(data, "N")
+    df_seq = table_sep(data, "S")
+    plot_node_stats(df_node, args.output, "#Nodes", "N")
+    plot_node_stats(df_seq, args.output, "#Sequence [bp]", "S")
--- src/path_similarity/writer_test.rs
+++ src/path_similarity/writer_test.rs
@@ -9,27 +9,18 @@ pub fn write_ps(data: &Vec<(String, Vec<(u32, u32)>)>, filename: &str) {
     let mut f = get_writer(filename).expect("Not able to write");
     let f1 = data[0].1.len();
 
-    let k = "Accession";
-    let fk: Vec<String> = (0..f1)
-        .map(|n| {
-            let mut d = "Node:".to_string();
-            d.push_str(&n.to_string());
-            d
-        })
-        .collect();
-    let fk1: Vec<String> = (0..f1)
-        .map(|n| {
-            let mut d = "Seq:".to_string();
-            d.push_str(&n.to_string());
-            d
-        })
-        .collect();
-    writeln!(f, "{}\t{}\t{}", k, fk.join("\t"), fk1.join("\t")).expect("Not able to write");
+    let k = "Accession\tType";
+    let fk = (0..f1).map(|n| n.to_string()).collect::<Vec<String>>().join("\t");
+    writeln!(f, "{}\t{}", k, fk).expect("Not able to write");
 
     for entry in data.iter() {
         let test1: Vec<String> = entry.1.iter().map(|n| n.0.to_string()).collect();
+        writeln!(f, "{}\tN\t{}", entry.0, test1.join("\t"))
+            .expect("Not able to write");
+    }
+    for entry in data.iter() {
         let test2: Vec<String> = entry.1.iter().map(|n| n.1.to_string()).collect();
-        writeln!(f, "{}\t{}\t{}", entry.0, test1.join("\t"), test2.join("\t"))
+        writeln!(f, "{}\tS\t{}", entry.0, test2.join("\t"))
             .expect("Not able to write");
     }
 }
